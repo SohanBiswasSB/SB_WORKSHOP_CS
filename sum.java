5db652d2cf7efc6541311e35a6b4d8798af78a76
SB <48861515+SohanBiswasSB@users.noreply.github.com>
2019-03-28 14:08:10 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.*;
+public class sum
+	{
+		public static void main(String args[])throws IOException
+		{
+			Scanner in = new Scanner(System. in);
+			System.out.println("ENTER 2 NUMBERS: ");
+			int a=in.nextInt();
+			int b=in.nextInt();
+			int sum=a+b;
+			System.out.println("SUM IS: "+sum);
+		}
+
+	}

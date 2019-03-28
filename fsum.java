c3d52b6d82c535910697631cef775a7dfcf8da39
SB <48861515+SohanBiswasSB@users.noreply.github.com>
2019-03-28 14:09:57 +0530

    Create fsum.java

---
+++ fsum.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.*;
+public class fsum
+	{
+	public static void main(String args[])throws IOException
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("ENTER HOW MANY TIMES YOU WANT TO RUN: ");
+		int n=in.nextInt();
+		int k=0;
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("ENTER THE NUMBER: ");
+			int a=in.nextInt();
+			k=fsum(a,n);
+		}
+		System.out.println("THE SUM IS: "+k);
+	}
+		static int fsum(int x, int z)
+		{
+			int sum=0;
+			for(int i=0;i<=z;i++)
+			{
+				sum+=x;
+				
+			}
+			return sum;
+		}
+	
+}
